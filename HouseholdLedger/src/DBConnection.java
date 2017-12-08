b9798a9570955bae6f14ef54c99760e86d7364bb
483759 <483759@naver.com>
2017-12-08 20:29:42 +0900

    Chart Drawing

--- HouseholdLedger/src/DBConnection.java
+++ HouseholdLedger/src/DBConnection.java
@@ -12,9 +12,6 @@ class DBMain{
 	static String user = "root";
 	static String pass = "426795";
 	int presentid = 0;
-	int[] cat = {0, 0, 0, 0, 0, 0, 0, 0};
-	//int inc = 0;
-	//int exp = 0;
 	Connection conn = null;
 	Statement st = null;
 
@@ -36,29 +33,32 @@ class DBMain{
 		}
 
 	}
-	
+
 
 	public void inquiry() throws Exception{	//DB에 저장된 정보를 불러오는 함수
 		ResultSet rs = null;
 
 		try {
 			st = conn.createStatement();
-			rs = st.executeQuery("SELECT id, date, money, category, note FROM user");
+			rs = st.executeQuery("SELECT id, date, category, money, sort, note FROM user");
 			//user테이블의 컬럼들을 불러온다
 
 			while(rs.next()) {
 				int id = rs.getInt("id");
 				String date = rs.getString("date");
-				int money = rs.getInt("money");
 				String ctg = rs.getString("category");
+				int money = rs.getInt("money");
+				int srt = rs.getInt("sort");
 				//String note = rs.getString("note");
 
 				System.out.print("id : " + id);
 				System.out.print(", date : " + date);
+				System.out.print(", category : " + ctg);
 				System.out.print(", money : " + money);
-				System.out.println(", category : " + ctg);
+				System.out.println(", sort : " + (srt==0?"수입":"지출"));
 
-				presentid = id;
+				if(presentid<id)
+					presentid = id;
 			}	//테이블의 항목들을 출력
 
 			rs.close();
@@ -70,6 +70,7 @@ class DBMain{
 	public void insert() throws Exception{	//DB에 새 정보를 삽입하는 함수
 		int rs2;
 		int id=0;
+		int srt=0;
 		String date="";
 		int money=0;
 		String category="";
@@ -80,10 +81,12 @@ class DBMain{
 			id = ++presentid;
 			System.out.print("날짜를 입력하세요 : ");
 			date = in.readLine();
-			System.out.print("금액을 입력하세요 : ");
-			money = Integer.parseInt(in.readLine());
 			System.out.print("항목을 입력하세요 : ");
 			category = in.readLine();
+			System.out.print("금액을 입력하세요 : ");
+			money = Integer.parseInt(in.readLine());
+			System.out.print("구분을 입력하세요(수입-0/지출-1) : ");
+			srt = Integer.parseInt(in.readLine());
 			System.out.print("메모를 입력하세요 : ");
 			note = in.readLine();
 		}catch(Exception e) {
@@ -91,8 +94,8 @@ class DBMain{
 		}
 
 		try {	//입력한 데이터를 DB의 user테이블에 삽입
-			String sql = "INSERT INTO user(id, date, money, category, note)";
-			sql += "VALUES(" + "'" + id + "','" + date + "','" + money + "','" + category + "','" + note + "');";
+			String sql = "INSERT INTO user(id, date, category, money, sort, note)";
+			sql += "VALUES(" + "'" + id + "','" + date + "','" + category + "','" + money + "','" + srt + "','" + note + "');";
 			st.executeUpdate(sql);
 
 		}catch(SQLException e) {
--- HouseholdLedger/src/Main.java
+++ HouseholdLedger/src/Main.java
@@ -6,7 +6,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = null;
 		
+		str=Integer.toString(10);
+		
+		System.out.println(str);
 		
 		//System.out.println("날짜 : "+date+" 금액 : "+money+" 항목 : "+category);
 	}
