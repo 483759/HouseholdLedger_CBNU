261288e0875b17cbd03ec70236a7eb6bd9e0fb8e
김한별 <@192.168.1.185>
2017-12-09 21:01:34 +0900

    1209_9:01

--- 2016039048_1203/src/JPanelTest.java
+++ 2016039048_1203/src/JPanelTest.java
@@ -1,5 +1,11 @@
 //패널전환
 
+import java.awt.BorderLayout;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.Choice;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +18,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
-import plus.*;
+
+//import plus.InputForm;
  
  class JPanel01 extends JPanel{ 
 	 
@@ -26,10 +33,13 @@ import plus.*;
 	 private JButton eraseBtn;
 
 	 private JScrollPane jScrollPane1;
+	// private JScrollPane jscroll1;
 	 private JTable jTable1;
+	 //private JTextField jtext;
 	 private JFrame frame1;
+	 
 	 private JPanelTest win;
-	 InputForm form;
+	 //InputForm form;
 
 	 public JPanel01(JPanelTest win) {
 		 this.win=win;
@@ -100,13 +110,95 @@ import plus.*;
 		
 	}
 */
-	 
-class MyActionListener7 implements ActionListener{
+class MyActionListener5 implements ActionListener{
 	@Override
+	//작성버튼 클릭시
 	public void actionPerformed(ActionEvent e) {
-		win.change("panel01");
+		
+		
+		
+		frame1=new JFrame("작성");
+		frame1.setLocation(500, 400);
+		frame1.setSize(400,400);
+		
+		Container contentPane = frame1.getContentPane();
+		//income = new JCheckBox("수입");
+		//expense = new JCheckBox("지출");
+		
+		frame1.setLayout(new FlowLayout());
+		Choice ch1 = new Choice();
+		ch1.addItem("외식");
+		ch1.addItem("카페/간식");             /////////////// 여러개중에 한개 선택 (ex 항목)
+		ch1.addItem("술/유흥");
+		ch1.addItem("생활");
+		ch1.addItem("쇼핑");
+		ch1.addItem("미용");
+		ch1.addItem("교통");
+		ch1.addItem("문화");
+		
+		Choice ch2 = new Choice();
+		ch2.addItem("2016");	ch2.addItem("2017");	ch2.addItem("2018");
+		
+		Choice ch3 = new Choice();
+		ch3.addItem("1");	ch3.addItem("2");	ch3.addItem("3");
+		ch3.addItem("4");	ch3.addItem("5");	ch3.addItem("6");
+		ch3.addItem("7");	ch3.addItem("8");	ch3.addItem("9");
+		ch3.addItem("10");	ch3.addItem("11");	ch3.addItem("12");
+		
+		Choice ch4 = new Choice();
+		ch4.addItem("1");	ch4.addItem("2");	ch4.addItem("3");
+		ch4.addItem("4");	ch4.addItem("4");	ch4.addItem("6");
+		ch4.addItem("7");	ch4.addItem("8");	ch4.addItem("9");
+		ch4.addItem("10");	ch4.addItem("11");	ch4.addItem("12");
+		ch4.addItem("13");	ch4.addItem("14");	ch4.addItem("15");
+		ch4.addItem("16");	ch4.addItem("17");	ch4.addItem("18");
+		ch4.addItem("19");	ch4.addItem("20");	ch4.addItem("21");
+		ch4.addItem("22");	ch4.addItem("23");	ch4.addItem("24");
+		ch4.addItem("25");	ch4.addItem("26");	ch4.addItem("27");
+		ch4.addItem("28");	ch4.addItem("29");	ch4.addItem("30");
+		ch4.addItem("31");	
+		
+		
+		
+		//frame1.add(income);
+		//frame1.add(expense);
+		contentPane.add(ch2,BorderLayout.CENTER);
+		contentPane.add(ch3,BorderLayout.EAST);
+		contentPane.add(ch4,BorderLayout.SOUTH);
+		contentPane.add(ch1,BorderLayout.NORTH);
+		
+		CheckboxGroup group = new CheckboxGroup();
+		Checkbox income = new Checkbox("수입",group,false);
+		Checkbox exepense = new Checkbox("지출",group,false);
+		
+		contentPane.add(income,BorderLayout.LINE_END);
+		contentPane.add(exepense,BorderLayout.LINE_END);
+		//JButton yes = new JButton();
+		//yes.setSize(20,10);
+		//yes.setLocation(30,30);
+		
+		
+		/*jscroll1 = new JScrollPane(jtext);
+		jscroll1.setSize(100,100);
+		jscroll1.setLocation(10,100);*/
+		//frame1.add();
+		
+		//jtext.setBounds(10,200,30,21);
+		//jtext.setLocation(30,30);
+		//jtext.setColumns(10);
+		
+		
+		
+		
+		
+		
+		frame1.setVisible(true);
+		
+		// frame1.add(jtext);
+		
 	}
-			
+	
+		
 }
 class MyActionListener6 implements ActionListener{
 	@Override
@@ -116,19 +208,15 @@ class MyActionListener6 implements ActionListener{
 	}
 			
 }
-class MyActionListener5 implements ActionListener{
+	 
+class MyActionListener7 implements ActionListener{
 	@Override
-	//작성버튼 클릭시
 	public void actionPerformed(ActionEvent e) {
-		frame1=new JFrame("작성");
-		frame1.setSize(300,300);
-		frame1.setVisible(true);
-		
-		
+		win.change("panel01");
 	}
-	
-		
+			
 }
+
 class MyActionListener1 implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -394,19 +482,19 @@ class JPanel02 extends JPanel{
 		jButton3.addActionListener(new MyActionListener3());
 		jButton4.addActionListener(new MyActionListener4());
 	}
-	
+		class MyActionListener1 implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			win.change("panel01");
+		}
+	}
 	class MyActionListener2 implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			win.change("panel02");
 		}
 	}
-	class MyActionListener1 implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			win.change("panel01");
-		}
-	}
+
 	class MyActionListener3 implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
